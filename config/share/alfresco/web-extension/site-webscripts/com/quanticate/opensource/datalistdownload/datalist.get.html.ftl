1851e4b5032fff430fab37222205061c9336e8d5
Muralidharan Dheenadhayalan <Muralidharan.Dheenadhayalan@quanticate.com>
2012-08-31 20:19:34 +0530

    Displaying current site's data lists

--- config/share/alfresco/web-extension/site-webscripts/com/quanticate/opensource/datalistdownload/datalist.get.html.ftl
+++ config/share/alfresco/web-extension/site-webscripts/com/quanticate/opensource/datalistdownload/datalist.get.html.ftl
@@ -1,10 +1,18 @@
-<#assign site=page.url.templateArgs.site>
 <div class="dashlet">
 	<div class="title">
 		Datalist Export
 	</div>
 	<div class="body scrollableList">
-	   Hello World
+	   <#list datalists as datalist>		
+         <div>
+            <div id="list">
+				 <a href="${url.context}/page/site/${site}/data-lists?list=${datalist.name?html}">
+				 ${(datalist.title!"")?html}
+				 </a>			   
+               <div class="description">${(datalist.description!"")?html}</div>
+            </div>        
+		</div>
+		</#list>
     </div>
 </div>
 
--- config/share/alfresco/web-extension/site-webscripts/com/quanticate/opensource/datalistdownload/datalist.get.js
+++ config/share/alfresco/web-extension/site-webscripts/com/quanticate/opensource/datalistdownload/datalist.get.js
@@ -1,6 +1,32 @@
 function main()
 {
-	
+	var site;
+	if (args.site)
+	{
+	   site = args.site;
+	}
+	else
+	{
+	   site = page.url.templateArgs.site;
+	}
+
+	  theUrl = "/slingshot/datalists/lists/site/test/dataLists?page=1&pageSize=512",
+      result = remote.call(theUrl),
+      canCreate = false,
+      datalists = [];
+   
+   if (result.status == 200)
+   {
+      response = eval('(' + result.response + ')');
+      datalists = response.datalists;  
+      canCreate = response.permissions.create;
+	  model.datalists = datalists;
+	  
+   }
+	model.site = site;
+    
+
+
 }
 
 main();
